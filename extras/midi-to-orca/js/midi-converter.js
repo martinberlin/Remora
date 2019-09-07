4e0025c3a524debe524f8e0e811cc96d9fc38515
martinberlin <martin@luckycloud.de>
2019-09-07 20:11:09 +0200

    1.0.0 version: The note-chopper

--- extras/midi-to-orca/js/midi-converter.js
+++ extras/midi-to-orca/js/midi-converter.js
@@ -1,6 +1,6 @@
 let midi = {};
 
-notemap = {'A':'A', 'B':'B', 'C':'C', 'D':'D', 'E':'E', 'F':'F', 'G':'G',
+let notemap = {'A':'A', 'B':'B', 'C':'C', 'D':'D', 'E':'E', 'F':'F', 'G':'G',
          'A#':'a', 'B#':'b', 'C#':'c', 'D#':'d', 'E#':'e', 'F#':'f', 'G#':'g',  };
 
 /**
@@ -8,38 +8,42 @@ notemap = {'A':'A', 'B':'B', 'C':'C', 'D':'D', 'E':'E', 'F':'F', 'G':'G',
  * @param {*} midi 
  */
 function midiOut(trackId = 1) {
-
+    let notetimes = [];
+    let notenotes = [];
     let tracks = $("div#midi-tracks"),
     settings   = $("div#midi-settings"),
     octavesOut = $("#orca-octaves"),
-    notesOut = $("#orca-notes"),
-    bpmEl  = $("input#bpm"),
-    colsEl = $("input#cols"),
-    wrap = $("input#wrap"),
-    trackEl = $("input:radio")
-    
+    notesOut   = $("#orca-notes"),
+    bpmEl      = $("input#bpm"),
+    colsEl     = $("input#cols"),
+    wrap       = $("input#wrap"),
+    silenced   = $("input#silence-detect"),
+    trackEl    = $("input:radio")
+    ;
     // Clean out output areas
-    octavesOut.val('')
-    notesOut.val('')
-
-    // If promise is done
+    octavesOut.val('');
+    notesOut.val('');
+    
     bpm  = bpmEl.val();
     cols = colsEl.val();
-    //console.log(midi.header);
+    //console.log(midi.header); // Note: Not all midis have a tempo header
     midiHeaderTempos = midi.header.tempos;
     midiBpm = (midiHeaderTempos.length) ? Math.round(midiHeaderTempos[0].bpm) : 0;
     console.log("ORCΛ settings BPM:"+bpm+" COLS:"+cols+" / Midi BPM: "+midiBpm);
     let trackNr = 1;
+    // Millis per beat from BMP calculation
+    let mb = Math.round(60000/bpmEl.val());
+    let trackMillis = 0;
     let orcaOctaves = "";
     let orcaNotes   = "";
     tracks.html('<b>Midi select track</b><br>');
 
     outPrefix  = (wrap.is(":checked")) ? '#' :'';
     outPrepend = (wrap.is(":checked")) ? '#' :'';
-
+    
     // Read the tracks
     midi.tracks.forEach(track => {
-        //tracks have notes and controlChanges
+        //Tracks have notes and controlChanges for the moment we ignore the second ones
         if (trackId === trackNr) {
         console.log("Processing: "+track.instrument.name);
         //notes are an array
@@ -57,8 +61,7 @@ function midiOut(trackId = 1) {
                 console.log(noteTime, note.name);
                 debug ++; 
             }
-            // Extract Note 
-            console.log('Note: '+note.name);
+            // Extract Note // console.log('Note: '+note.name);
             // Check if # Sharp note 
             if (note.name.substr(1,1) === '#') {
                 // Extract sharp
@@ -68,26 +71,87 @@ function midiOut(trackId = 1) {
                 notesel = note.name.substr(0,1);
                 octasel = note.name.substr(1,1);
             }
-            console.log(' orca: '+notemap[notesel]+' '+octasel);
-            // Most important part is the note falling into the bpm grid or is a silence            
-            orcaNotes += (notesel in notemap) ? notemap[notesel] : '';
+            // console.log(' orca: '+notemap[notesel]+' '+octasel);
+            // Detect if the note lands into the output grid or there is a silence
+            // The most important part of the music is the silence
             orcaOctaves += octasel;
-            
-            
-            // Jump to next row
+            // We will not calculate silence for octaves 
             if (notesCnt % cols == 0)  {
-                notesOut.val(notesOut.val()+outPrefix+orcaNotes+outPrepend+"\n");
                 octavesOut.val(octavesOut.val()+outPrefix+orcaOctaves+outPrepend+"\n");
-                orcaNotes = '';
                 orcaOctaves = '';
             }
+
+            if (silenced.is(":checked")) {
+                // We generate the array here but output notes later
+                n = (notesel in notemap) ? notemap[notesel] : '';
+                notetimes.push(noteTime);
+                notenotes.push(n);
+            } else {
+                // All notes together no silence is taken in account
+                orcaNotes += (notesel in notemap) ? notemap[notesel] : '';
+
+                if (notesCnt % cols == 0)  {
+                    notesOut.val(notesOut.val()+outPrefix+orcaNotes+outPrepend+"\n");
+                    orcaNotes = '';
+                }
+            }           
             
             notesCnt++; 
         });
+      
+        // All this will happen if detect silences is checked
+        if (silenced.is(":checked")) {
+        lastnotetime = (notetimes[notetimes.length-1]);
+        /* We iterate a stepper jumps millis per beat at a time from 0 to lastnotetime
+           that goes from initial to last note and cherry picks the note or silence
+        */
+        let lastIndex = notetimes[0];
+
+        // We have two counters in place. NoteCnt is the notes index
+        // gridIndex is the position in the grid
+        notesCnt = 0; 
+        gridIndex = 1;
+        orcaNotes = '';
+        gridLines = 1;
+        console.log('Last note time: '+lastnotetime);
+        console.log(notetimes);
+        console.log(notenotes);
+
+        let degugTo = 100;
+        for (var i = notetimes[0]; i <= lastnotetime; i+=mb) {
+          if (gridIndex<degugTo) console.log("noteTime:"+notetimes[notesCnt]+">="+lastIndex+" && <= "+i);
+
+          if (notetimes[notesCnt]>=lastIndex && notetimes[notesCnt]<=i) {
+              // Note lands in grid
+              orcaNotes += notenotes[notesCnt];
+              if (gridIndex<degugTo) console.log(notenotes[notesCnt]+" at time:" +i+ " Note count: "+notesCnt);
+              notesCnt++;
+
+          } else {
+              // We detected a silence
+              if (gridIndex<degugTo) console.log('.');
+              orcaNotes += ".";
+              // This will skip notes to maintein time (Bad option, but there is any other?)
+              while (i>notetimes[notesCnt]) {
+                  notesCnt++;
+              }
+          }
+          // Jump to next row if Cols match
+          if (gridIndex % cols == 0)  {
+              console.log('%'+cols+' matched. orcaNotes:'+orcaNotes);
+            notesOut.val(notesOut.val()+outPrefix+orcaNotes+outPrepend+"\n");
+            orcaNotes = '';
+            if (gridIndex<degugTo) console.log('LINE '+gridLines+'________________');
+            gridLines++;
+          }
+          lastIndex = i;
+          gridIndex++;
+        }
+    } // Silence mapping
         
-        
-    }
-        // the track also has a channel and instrument
+    } // End of track loop
+
+        // The track also has a channel and instrument
         trackChecked = (trackNr === trackId) ? "checked" : "";
         trackRadio = $('<input type="radio" name="track" value="'+trackNr+'" onchange="midiOut('+trackNr+')" id="t'+trackNr+'" '+trackChecked+' /> <label for="t'+trackNr+'">'+ track.instrument.name +'</label><br>');
             tracks.append(trackRadio);

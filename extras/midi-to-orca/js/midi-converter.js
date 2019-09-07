644a82655ecc87b88527f65c10c31ef7607495ef
martinberlin <martin@luckycloud.de>
2019-09-07 10:37:06 +0200

    Stable selecting midi track

--- extras/midi-to-orca/js/midi-converter.js
+++ extras/midi-to-orca/js/midi-converter.js
@@ -1,46 +1,71 @@
+let midi = {};
+/**
+ * Outputs the midi into Orca format
+ * @param {*} midi 
+ */
+function midiOut(trackId = 1) {
+    let tracks = $("div#midi-tracks"),
+    settings = $("div#midi-settings"),
+    octaves = $("div#midi-octave"),
+    chords = $("div#midi-chords"),
+    bpmEl  = $("input#bpm"),
+    colsEl = $("input#cols"),
+    rowsEl = $("input#rows"),
+    trackEl = $("input:radio")
+    ;
+    // If promise is done
+    bpm  = bpmEl.val();
+    cols = colsEl.val();
+    rows = rowsEl.val();
+    midiHeaderTempos = midi.header.tempos;
+    midiBpm = Math.round(midiHeaderTempos[0].bpm);
+    console.log("ORCΛ settings BPM:"+bpm+" COLS:"+cols+" ROWS:"+rows);
+    console.log("Midi BPM: "+midiBpm);
+    
+    //the file name decoded from the first track
+    const name = midi.name;
+    let trackNr = 1;
+    tracks.html('<b>Midi select track</b><br>');
+
+    //get the tracks
+    midi.tracks.forEach(track => {
+        //tracks have notes and controlChanges
+        if (trackId === trackNr) {
+
+        //notes are an array
+        const notes = track.notes;
+        console.log("Note    Time     Dur.    Name");
+        let debug = 1; 
+        notes.forEach(note => {
+            noteTime = Math.round(1000*note.time);
+            if (debug<=3) {
+                console.log(note);
+                debug ++; 
+            } 
+            if (debug<=4) {
+                console.log(noteTime, note.name);
+                debug ++; 
+            }
+            //console.log(note.midi, note.time, note.duration, note.name);
+        });
+        
+    }
+        // the track also has a channel and instrument
+        console.log(track.instrument.name);
+        trackChecked = (trackNr === trackId) ? "checked" : "";
+        trackRadio = $('<input type="radio" name="track" value="'+trackNr+'" onchange="midiOut('+trackNr+')" id="t'+trackNr+'" '+trackChecked+' /> <label for="t'+trackNr+'">'+ track.instrument.name +'</label><br>');
+            tracks.append(trackRadio);
+            trackNr++;
+        });
+    }
+
 $(document).ready(function() {
+
 // Load a midi file in the browser. Demo: old-town-road.mid samba-pa-ti.mid
     const midiPromise = Midi.fromUrl("midis/tiersen_amelie.mid"); // Returns promise
-
-    let tracks = $("div#midi-tracks"),
-        settings = $("div#midi-settings"),
-        octaves = $("div#midi-octave"),
-        chords = $("div#midi-chords"),
-        bpmEl  = $("input#bpm"),
-        colsEl = $("input#cols"),
-        rowsEl = $("input#rows")
-        ;
-
-    midiPromise.then(function (midi) {
-
-        // If promise is done
-        bpm  = bpmEl.val();
-        cols = colsEl.val();
-        rows = rowsEl.val();
-        console.log("ORCΛ settings BPM:"+bpm+" COLS:"+cols+" ROWS:"+rows);
-        console.log(midi);
-
-        //the file name decoded from the first track
-        const name = midi.name;
-        let trackNr = 1;
-
-        //get the tracks
-        midi.tracks.forEach(track => {
-            //tracks have notes and controlChanges
-
-            //notes are an array
-            const notes = track.notes;
-            console.log("Note    Time     Dur.    Name");
-            notes.forEach(note => {
-                console.log(note.midi, note.time, note.duration, note.name);
-            });
-
-            //the track also has a channel and instrument
-            if (trackNr===1) tracks.append("<b>Midi tracks</b><br>");
-            console.log(track.instrument.name);
-            tracks.append(trackNr + ".- " + track.instrument.name +"<br>");
-            trackNr++;
-        })
+    midiPromise.then(function (midiIn) {
+        midi = midiIn;
+        midiOut();
     });
-
 });
+

52ef7b5242d17aa441d7bb5087515ebed9a4d89c
martinberlin <martin@luckycloud.de>
2019-10-03 11:35:25 +0200

    Adding @iotpanic Pixels library. Will receive pixels using his protocol when UDP payload>9

--- lib/Animate/Animate.cpp
+++ lib/Animate/Animate.cpp
@@ -27,6 +27,8 @@ int signalLevelMajorThan = -50;
 uint8_t BUFFER[READ_LEN] = {0};
 uint16_t *adcBuffer = NULL;
 
+// Output class receives binary and outputs to Neopixel
+Animate pix;
 // Sent from main.cpp:
 struct config {
   int udpPort; 
@@ -346,6 +348,13 @@ void Animate::startUdpListener(const IPAddress& ipAddress, int udpPort) {
 
     // Callback that gets fired every time an UDP Message arrives
     udp.onPacket([](AsyncUDPPacket packet) {
+
+        if (packet.length()>9) {
+            //Serial.println("Call pixels->receive()");
+            pix.receive(packet.data(), packet.length());
+            return;
+        }
+
         if(debugMode) {
             Serial.print("Data L "+String(packet.length())+" : ");
             Serial.write(packet.data(), packet.length());Serial.println();
@@ -598,4 +607,103 @@ void Animate::loop() {
     if (enableBeatDetection) {
         Animate::micRead();
     }
+}
+
+
+// @iotPanic Pixels library
+bool Animate::receive(uint8_t *pyld, unsigned length){
+    uint16_t pixCnt = 0;
+    pixel *pattern = unmarshal(pyld, length, &pixCnt);
+    if(pixCnt==0){
+        Serial.println("Returning from failed marshal");
+        Serial.println("Received length: "+String(length));
+        return false;
+    }
+    
+    /*
+    for(uint i=0; i<pixCnt; i++){
+        if (pattern[i].R+pattern[i].B+pattern[i].G) {
+        Serial.print("LED index:"+String(i)+" RGB(");
+        Serial.print(pattern[i].R);
+        Serial.print(",");
+        Serial.print(pattern[i].G);
+        Serial.print(",");
+        Serial.print(pattern[i].B);
+        Serial.println(")");
+        }
+    } */
+    
+    this->show(pattern, pixCnt);
+    delete pattern;
+    return true;
+}
+
+void Animate::write(unsigned location, uint8_t R, uint8_t G, uint8_t B, uint8_t W){
+    #ifdef RGBW
+    strip.setPixelColor(location, RgbwColor(R,G,B,W));
+    #else
+    strip.SetPixelColor(location, RgbColor(R,G,B));
+    #endif
+}
+
+void Animate::show(){
+    strip.Show();
+}
+
+void Animate::show(pixel *pixels, unsigned cnt){
+    for(unsigned i = 0; i<cnt; i++){
+        #ifdef RGBW
+        strip.setPixelColor(i, RgbwColor(pixels[i].R,pixels[i].G,pixels[i].B,pixels[i].W));
+        #else
+        strip.SetPixelColor(i, RgbColor(pixels[i].R,pixels[i].G,pixels[i].B));
+        #endif
+    }
+    strip.Show();
+}
+
+pixel *Animate::unmarshal(uint8_t *pyld, unsigned len, uint16_t *pixCnt, uint8_t *channel){
+    if(pyld[0]!=0x50){
+        Serial.println("Missing checkvalue, instead got: ");
+        Serial.print(pyld[0], HEX);
+        // Set pixCnt to zero as we have not decoded any pixels and return NULL
+        *pixCnt = 0;
+        return NULL;
+    }
+    //if (pyld[1]!=syncWord||syncWord==0x0){
+    //    *pixCnt = 0;
+    //    return NULL;
+    //} TODO UNCOMMENT REMOVED FOR EASY NETCAT USAGE QUICKLY
+    if(channel!=NULL){
+        *channel = pyld[2];
+    }
+    // Decode number of pixels, we don't have to send the entire strip if we don't want to
+    uint16_t cnt = pyld[3] | pyld[4]<<8;
+    if(cnt>PixelCount){
+        // We got more pixels than the strip allows
+        *pixCnt = 0;
+        return NULL;
+    }
+    if (cnt ==0)
+    {
+        return false;
+    }
+    pixel *result = new pixel[cnt];
+    // TODO Add logic to return if len is impossibly large or small
+    for(uint16_t i = 0; i<cnt; i++){
+        #ifdef RGBW
+        result[i].R = pyld[5+(i*4)];
+        result[i].G = pyld[5+(i*4)+1];
+        result[i].B = pyld[5+(i*4)+2];
+        result[i].W = pyld[5+(i*4)+3]
+        #else
+        result[i].R = pyld[5+(i*3)];
+        result[i].G = pyld[5+(i*3)+1];
+        result[i].B = pyld[5+(i*3)+2];
+        #endif
+    }
+
+
+    // TODO Add CRC check before setting pixCnt
+    *pixCnt = cnt;
+    return result; 
 }
--- lib/Animate/Animate.h
+++ lib/Animate/Animate.h
@@ -8,8 +8,16 @@
 // Microfone Clock and Data pin
 #define PIN_CLK  0
 #define PIN_DATA 34
-
 #include <driver/i2s.h>
+
+typedef struct{
+    uint8_t R = 0;
+    uint8_t G = 0;
+    uint8_t B = 0;
+    #ifdef RGBW
+    uint8_t W = 0;
+    #endif
+} pixel;
 
 class Animate
 {
@@ -23,5 +31,24 @@ class Animate
 
     void loop();
     
+        // receive requires a pointer to a uint8_t array and the length of the array from a callback function
+    bool receive(uint8_t *pyld, unsigned len);
+    // sync will return the sync byte as a uint8_t to ensure the library and controller are on the same page
+    uint8_t sync();
+    // Write sets the LED at the location to R,G,B to the values provided for the next show()
+    void write(unsigned location, uint8_t R, uint8_t G, uint8_t B, uint8_t W = 0);
+    // Show writes the previously made write() calls to the array of LEDs
+    void show();
+    // This version of show takes a pointer to an array of pixels, as well as how long the array is. Be sure the array is in order from LED location 0 onward
+    // There is another versio that will accapt a single pixel and location
+    void show(pixel *pixels, unsigned cnt);
+
+    private:
+    // unmarshal returns a pointer to an array of pixels and accepts a pointer to a uint8_t array payload with the length of the array, as
+    // well as a pointer to an unsigned integer which will be changed to the number of LEDs decoded from the payload. If invalid a NULL will
+    // be returned and the value at pixCnt will be set to zero.
+    pixel *unmarshal(uint8_t *pyld, unsigned len, uint16_t *pixCnt, uint8_t *channel=NULL);
+
+    uint8_t syncWord = 0x0;
 };
 

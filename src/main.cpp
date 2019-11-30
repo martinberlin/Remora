bf7115be87d8cec17c38ca2ee85cf8cc32412ee7
martinberlin <martin@luckycloud.de>
2019-11-30 23:31:23 +0100

    Remove WiFi manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
-// Remora Firmware Beta 1.1 First squeleton
-
+// Remora Firmware WiFi Configuration over Bluetooth serial
 #include <WiFi.h>
 // Config.h and Animate.h need configuration
 #include <Config.h>  // WiFi credentials, mDNS Domain
@@ -7,8 +6,7 @@
 #include <ESPmDNS.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/timers.h"
-// Uncomment to use WiFi manager
-//#define USE_WIFI_MANAGER	
+
 
 // Debug mode prints to serial
 bool debugMode = true;
@@ -20,13 +18,6 @@ struct config {
   char chipId[20];
   int udpPort = 49161; // 49161 Default Orca UDP Port
 } internalConfig;
-
-#ifdef USE_WIFI_MANAGER
-	#include <DNSServer.h>
-	#include <WebServer.h>
-	#include <WiFiManager.h>
-    WiFiManager wifiManager;
-#endif
 
 /**
  * Generic message printer. Modify this if you want to send this messages elsewhere (Display)
@@ -47,36 +38,12 @@ void connectToWifi() {
   WiFi.begin(WIFI_SSID1, WIFI_PASS1);
 }
 
-#ifdef USE_WIFI_MANAGER
-	//flag for saving data
-	bool shouldSaveConfig = false;
-
-	//callback notifying us of the need to save config
-	void saveConfigCallback () {
-		Serial.println("Should save config");
-		shouldSaveConfig = true;
-	}
-#endif
-
 void setup()
 { 
   Serial.begin(115200);
   
-  //reset saved settings
-  //wifiManager.resetSettings();
- 
-	#if defined(USE_WIFI_MANAGER)
-	    WiFiManager wifiManager;
-		wifiManager.setTimeout(180);
-		wifiManager.setConfigPortalTimeout(180); // try for 3 minute
-		wifiManager.setMinimumSignalQuality(15);
-		wifiManager.setRemoveDuplicateAPs(true);
-		wifiManager.setSaveConfigCallback(saveConfigCallback);
-		wifiManager.autoConnect("RemoraAP");
-        Serial.println("Wifi Manager start");
-	    #else
-  		connectToWifi();
-  	#endif
+  	connectToWifi();
+  
 
 	  while (WiFi.status() != WL_CONNECTED) {
 		delay(300);

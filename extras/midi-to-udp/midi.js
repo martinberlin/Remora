e480378b92dfa2acbfb3457d7e744f38e4d86de2
martinberlin <martin@luckycloud.de>
2020-09-14 08:47:55 +0200

    #9 Blocker found: UDP.send() is not called when you call it inside the midi receive callback

--- extras/midi-to-udp/midi.js
+++ extras/midi-to-udp/midi.js
@@ -4,13 +4,15 @@
  */
 const yargs = require("yargs");
 const midi = require('midi');
+const dgram = require('dgram');
+var udpclient = dgram.createSocket('udp4');
 
+var DEBUG = true;
+var PORT = 49161;       // Default Orca UDP output port
 // Set up a new input.
 const input = new midi.Input();
-
-// Count the available input ports.
-input.getPortCount();
 // Print port names
+let udp_ip = "";
 let ports = "ID: Port name\n";
 for (let n=0; n<input.getPortCount(); n++){
   ports += n.toString() +': '+input.getPortName(n)+"\n";
@@ -19,33 +21,45 @@ for (let n=0; n<input.getPortCount(); n++){
 let options = yargs
  .usage("Usage: -p <MIDI_PORT_ID>")
  .option("p", { alias: "port_id", describe: ports, type: "number", demandOption: true })
+ .option("u", { alias: "udp_ip", describe: udp_ip, type: "string", demandOption: true })
  .argv;
 
-console.log("Listening to: "+input.getPortName(options.port_id));
+console.log("Listening to: "+input.getPortName(options.port_id)+ ' and forwarding to '+options.udp_ip+':'+PORT);
+
+// This send per UDP works
+sendPerUdp("Test");
+
 
 // Configure a callback.
 input.on('message', (deltaTime, message) => {
+  
   // The message is an array of numbers corresponding to the MIDI bytes: https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html
   if (message!='248') {
-  	console.log(message);
+    noteStatus = (message[0]==145) ? '1' : '0';
+    noteChord  = message[1].toString(16);  // FF 0 01
+    noteVelocity  = (message[2].toString(16).length==1) ?'0'+message[2].toString(16):message[2].toString(16);
+    let noteInfo = noteChord+noteStatus+noteVelocity;
+    console.log(noteInfo);
+
+    // When called inside the midi callback it simply does not work
+    sendPerUdp(noteInfo);
   }
 });
 
-// Open the first available input port.
+// Open the port received per command line -p parameter
 input.openPort(options.port_id);
-
-// Sysex, timing, and active sensing messages are ignored
-// by default. To enable these message types, pass false for
-// the appropriate type in the function below.
-// Order: (Sysex, Timing, Active Sensing)
-// For example if you want to receive only MIDI Clock beats
-// you should use
-// input.ignoreTypes(true, false, true)
 input.ignoreTypes(false, false, false);
 
-// ... receive MIDI messages ...
 
-// Close the port when done.
-//setTimeout(function() {
-//  input.closePort();
-//}, 200000);
+function sendPerUdp(noteInfo) {
+  // Send per UDP - is not sending 
+  var inBuffer = Buffer.from(noteInfo, 'utf8');
+  udpclient.send(inBuffer,PORT,options.udp_ip, function(err, bytes) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    
+    console.log('Sent: ' + inBuffer.toString() + ' to ' + options.udp_ip +':'+ PORT);
+  });
+}
